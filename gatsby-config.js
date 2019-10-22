8e4ccce3a3d74523769ad7c13c3362053d81cde0
jasonlingkkday <jason.ling@kkday.com>
2019-10-22 09:32:48 +0800

    commented out problemetic plugin feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-feed`,
+    // `gatsby-plugin-feed`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
